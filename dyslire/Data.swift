c1d19dbec69bfde55b19d91a30f7fbed35735c8d
rayen-bellali <124739899+rayen-bellali@users.noreply.github.com>
2023-11-09 03:55:25 +0100

    wleh t3ebet

---
+++ dyslire/Data.swift
@@ -0,0 +1,25 @@
+//
+//  Data.swift
+//  dyslire
+//
+//  Created by rayenbellali on 8/11/2023.
+//
+
+import SwiftUI
+
+var shopItems: [[Any]] = [
+    ["regle", "regle", 5.00, Color.purple],
+    ["tablette", "tablette", 550.00, Color.red],
+    ["rapid", "rapid", 50.00, Color.white],
+    ["pdf", "pdf", 20.00, Color.red],
+    ["tap", "tap", 80.00, Color.orange],
+    ["speech", "speech", 70.00, Color.white],
+    ["cahier", "cahier", 5.25, Color.gray],
+    ["clavier", "clavier", 90.50, Color.white],
+    ["stylo", "stylo", 6.50, Color.pink],
+    ["outils-lecture", "outils-lecture", 90.50, Color.yellow],
+    ["dragon", "dragon", 90.50, Color.black],
+]
+
+var cartItems: [[Any]] = [
+]
